767d717469209f89d703dc85538de1fe45f4e297
oliviastroeven <ostr298@aucklanduni.ac.nz>
2025-09-15 14:01:20 +1200

    Adding license info to all source code - both OY & SS for all code apart from extracted_bounded.py which is SS only

--- scripts/LAMPSEQ.sh
+++ scripts/LAMPSEQ.sh
@@ -3,6 +3,24 @@
 # LAMP Sequencing Bioinformatics Pipeline
 # Authors: O. Yugovich, S. Sturrock
 
+# This file is part of LAMPSEQ.
+# Copyright (C) 2025 Olivia Yugovich and Shane Sturrock
+#
+# LAMPSEQ is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ 
+
 # Fail on errors so the script doesn't continue
 #set -e
 
--- scripts/species_id/species_id.R
+++ scripts/species_id/species_id.R
@@ -1,5 +1,24 @@
 # LAMPSEQ - An analysis tool for LAMP sequencing data
 
+# This file is part of LAMPSEQ.
+# Copyright (C) 2025 Olivia Yugovich and Shane Sturrock
+#
+# LAMPSEQ is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# LAMP Sequencing Bioinformatics Pipeline
+# Authors: O. Yugovich, S. Sturrock
+
 
 # DESCRIPTION:
 # This script will read in the .bam files created by bowtie2, 
